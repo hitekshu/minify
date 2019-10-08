c5fed45a3e6cbe187387aa91df65ad692423da66
Hitekshu Paliwal <hpaliwal@entrata.com>
2019-10-08 08:22:28 -0600

    Added comments to Routes file

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,25 @@
 |
 */
 
-/*Route::get('/', function () {
-	return view('welcome');
-});*/
 
-//Route::auth();
+
+// Home Page
 Route::get('/', 'Controller@index')->name('home');
+
+// Authentication Routes
 Auth::routes();
+
+// Routes to Add/EditDelete short urls
 Route::get('/dashboard/shortUrl/create', 'Dashboard\ShortUrl\ShortUrlController@create')->name('createShortUrl');
 Route::get('/dashboard/shortUrl/edit', 'Dashboard\ShortUrl\ShortUrlController@edit')->name('editShortUrl');
 Route::get('/dashboard/shortUrl/delete', 'Dashboard\ShortUrl\ShortUrlController@delete')->name('deleteShortUrl');
-Route::get('/dashboard', 'Dashboard\DashboardController@loadDahsboard')->name('dashboard');
 Route::post('/dashboard/shortUrl/save', 'Dashboard\ShortUrl\ShortUrlController@save')->name('saveShortUrl');
+
+// Dashboard Home page Route
+Route::get('/dashboard', 'Dashboard\DashboardController@loadDahsboard')->name('dashboard');
+
+// Route for guests to generate minified URL
 Route::post('/', 'Controller@saveGuestUrl')->name('saveAsGuest');
+
+// Route that redirects all minified URL's to full urls
 Route::get('{code}', 'RedirectController@redirect')->name('redirect');
